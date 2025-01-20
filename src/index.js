f1446ca4f46ac2d744cc335232556bd064580bec
Tim Walkom <timwalkom@hotmail.com>
2025-01-20 16:13:07 +1030

    The buttons now become unactive when not needed
    Made a few changes to the buttons, get disabled
    when clicked or before they are needed.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import _ from 'lodash';
 import './style.css';
-import { openingPageContent, openingButtons } from './opening-page';
+import { openingPageContent, openingButtons, selection } from './opening-page';
 
+//In opening page
+//Will produce the content for the first page
 openingPageContent();
+
+//Event listeners etc for button pushes on opening page
+selection();
--- src/opening-page.js
+++ src/opening-page.js
@@ -5,8 +5,9 @@ export const openingButtons = {
     kilogramButton: false,
     caloriesButton: false,
     kilojoulesButton: false,
-    enterButton: false
 };
+
+const enterButtonSelection = false;
 
 const mainSection = document.querySelector('.mainSection');
 const container = mainSection.querySelector('.container');
@@ -22,23 +23,31 @@ function openingWriteUp () {
     container.appendChild(writeUpContainer);
 }
 
+//Building the selection buttons
 function selectionButtons(buttonOne, buttonTwo) {
     const openingButtonsContainer = document.createElement('div');
-    openingButtonsContainer.classList.add('ButtonsContainer');
+    openingButtonsContainer.classList.add('buttonsContainer');
     
     const firstButton = document.createElement('button');
     firstButton.classList.add(buttonOne + 'Button');
+    firstButton.classList.add('openPageButtons');
     firstButton.textContent = buttonOne;
     openingButtonsContainer.appendChild(firstButton);
 
+    const or = document.createElement('h1');
+    or.textContent = 'OR';
+    openingButtonsContainer.appendChild(or);
+
     const secondButton = document.createElement('button');
     secondButton.classList.add(buttonTwo + 'Button');
+    secondButton.classList.add('openPageButtons');
     secondButton.textContent = buttonTwo;
     openingButtonsContainer.appendChild(secondButton);
 
     container.appendChild(openingButtonsContainer);
 }
 
+//Building the select Button
 function enterButton() {
     const enterButtonContainer = document.createElement('div');
     enterButtonContainer.classList.add('enterButtonsContainer');
@@ -46,6 +55,8 @@ function enterButton() {
     const enterButton = document.createElement('button');
     enterButton.classList.add('enterButton');
     enterButton.textContent = 'Select';
+
+    enterButton.disabled = true;
 
     enterButtonContainer.appendChild(enterButton);
 
@@ -59,4 +70,67 @@ export function openingPageContent() {
     selectionButtons('Pounds', 'Kilograms');
     selectionButtons('Calories',  'Kilojoules');
     enterButton();
+}
+
+
+
+//Creating the functionality of the buttons
+export function selection() {
+    let buttons = document.querySelectorAll('.openPageButtons');
+
+    buttons.forEach((button) => {
+        button.addEventListener('click', (event) => {
+            if(event.target.classList.contains('PoundsButton')) {
+                openingButtons.poundsButton = true;
+                openingButtons.kilogramButton = false;
+                console.log(openingButtons);
+            } else if (event.target.classList.contains('KilogramsButton')) {
+                openingButtons.poundsButton = false;
+                openingButtons.kilogramButton = true;
+                console.log(openingButtons);
+            } else if (event.target.classList.contains('CaloriesButton')) {
+                openingButtons.caloriesButton = true;
+                openingButtons.kilojoulesButton = false;
+                console.log(openingButtons);
+            } else if (event.target.classList.contains('KilojoulesButton')) {
+                openingButtons.caloriesButton = false;
+                openingButtons.kilojoulesButton = true;
+                console.log(openingButtons);
+            }
+            changeButtonStatus();
+        })
+    })
+}
+//This function will disable the chosen options button.
+function changeButtonStatus() {
+    let poundsButton = document.querySelector('.PoundsButton');
+    let kilogramsButton = document.querySelector('.KilogramsButton');
+    let caloriesButton = document.querySelector('.CaloriesButton');
+    let kilojoulesButton = document.querySelector('.KilojoulesButton');
+    let selectButton = document.querySelector('.enterButton');
+    let count = 0;
+
+    if (openingButtons.poundsButton === true) {
+        poundsButton.disabled = true;
+        kilogramsButton.disabled = false;
+        count++;
+    } else if (openingButtons.kilogramButton === true) {
+        poundsButton.disabled = false;
+        kilogramsButton.disabled = true;
+        count++;
+    }
+    if (openingButtons.caloriesButton === true) {
+        caloriesButton.disabled = true;
+        kilojoulesButton.disabled = false;
+        count++;
+    } else if (openingButtons.kilojoulesButton === true){
+        caloriesButton.disabled = false;
+        kilojoulesButton.disabled = true;
+        count++;
+    }
+     
+    //If 2 of the options are selected it will trigger a count of 2 resulting in the select button being activated.
+    if (count === 2) {
+        selectButton.disabled = false;
+    }
 }
